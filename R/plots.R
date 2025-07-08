5cb3881b001fc43cbac65146d7d53c143cc66b88
Erik Peteri <peteri.erik@gmail.com>
2025-07-08 14:34:42 +0300

    Create function to plot histogram of the samples

--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,7 @@
 trace_plot <- function(samples) {
   plot(seq(1:1000),  type = "l", col="blue" ,samples)
 }
+
+histogram <- function(samples_accepted, prob = TRUE) {
+  hist(samples_accepted)
+}
--- scripts/main.R
+++ scripts/main.R
@@ -7,6 +7,9 @@ result <- metropolis(target_density = unnormalized_gaussian)
 samples <- result$samples
 samples_accepted <- result$samples_accepted
 
+par(mfrow = c(1, 2))
 trace_plot(samples)
+histogram(samples_accepted)
+par(mfrow = c(1, 1))
 
 
