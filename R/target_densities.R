a32e47d7116f8d8208c706f65ee6d33d1d0137be
Erik Peteri <peteri.erik@gmail.com>
2025-07-09 14:54:33 +0300

    Add true density function for unnormalized gaussian mixture

--- R/target_densities.R
+++ R/target_densities.R
@@ -9,3 +9,9 @@ unnormalized_gaussian <- function(x) {
 unnormalized_gaussian_mixture <- function(x) {
   return(0.3 * exp(-((x + 3)^2) / (2 * 1^2)) + 0.7 * exp(-((x - 3)^2) / (2 * 0.5^2)))
 }
+
+gaussian_mixture <- function(x) {
+  component1 <- 0.3 * dnorm(x, mean = -3, sd = 1)
+  component2 <- 0.7 * dnorm(x, mean = 3, sd = 0.5)
+  return(component1 + component2)
+}
