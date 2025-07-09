60a9ee1f02bb2b585fd8302ce6ac5a3141642c86
Erik Peteri <peteri.erik@gmail.com>
2025-07-09 15:36:40 +0300

    Add proper labels to the plots

--- R/plots.R
+++ R/plots.R
@@ -1,8 +1,8 @@
 trace_plot <- function(samples) {
-  plot(seq(samples),  type = "l", col="dodgerblue" ,samples, xlab = "Iteration", ylab = "Sample")
+  plot(seq(samples), type = "l", col="dodgerblue" ,samples, xlab = "Iteration", ylab = "Sample", main = "Trace plot")
 }
 
 histogram <- function(samples_accepted, true_density) {
-  hist(samples_accepted, col="dodgerblue", prob = TRUE)
+  hist(samples_accepted, col="dodgerblue", prob = TRUE, main = "Histogram of accepted samples", xlab = "Sample")
   curve(true_density, add = TRUE, col = "red", lwd = 2)
 }
--- scripts/main.R
+++ scripts/main.R
@@ -3,14 +3,14 @@ source("R/plots.R")
 source("R/proposals.R")
 source("R/metropolis.R")
 
-result <- metropolis(target_density = unnormalized_gaussian_mixture, proposal = gaussian_proposal)
+result <- metropolis(target_density = unnormalized_gaussian, proposal = gaussian_proposal)
 
 samples <- result$samples
 samples_accepted <- result$samples_accepted
 
 par(mfrow = c(1, 2))
 trace_plot(samples)
-histogram(samples_accepted, gaussian_mixture)
+histogram(samples_accepted, gaussian)
 par(mfrow = c(1, 1))
 
 
