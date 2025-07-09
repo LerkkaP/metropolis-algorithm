3c4249a3ae2782ee492fe75f32c28b33659ac642
Erik Peteri <peteri.erik@gmail.com>
2025-07-09 13:46:49 +0300

    Add unnormalized gaussian mixture target density function

--- R/target_densities.R
+++ R/target_densities.R
@@ -1,3 +1,7 @@
 unnormalized_gaussian <- function(x) {
   return(exp(-(x**2) / 2))
 }
+
+unnormalized_gaussian_mixture <- function(x) {
+  return(0.3 * exp(-((x + 3)^2) / (2 * 1^2)) + 0.7 * exp(-((x - 3)^2) / (2 * 0.5^2)))
+}
